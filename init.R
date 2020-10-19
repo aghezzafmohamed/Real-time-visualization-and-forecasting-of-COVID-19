bb5d9e7b670052b9fe81f38bd8b35b21aa17cd11
aghezzafmohamed <46581188+aghezzafmohamed@users.noreply.github.com>
2020-10-19 19:02:08 +0200

    first commit

--- init.R
+++ init.R
@@ -3,15 +3,12 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("forecast", "numberFormattR", "ggfortify", "shinydashboard", "plotly", "data.table", "lubridate", "tibble", "jsonlite", "httr")
+my_packages = c("forecast", "ggfortify", "shinydashboard", "plotly", "data.table", "lubridate", "tibble", "jsonlite", "httr")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
   }
 }
-if (!requireNamespace("devtools", quietly = TRUE))
-  install.packages("devtools")
-devtools::install_github('mraess/numberFormattR', force=TRUE)
 
 invisible(sapply(my_packages, install_if_missing))

2da8ba2031b4ee7a5c6e03fa2a3b3f225063035e
aghezzafmohamed <46581188+aghezzafmohamed@users.noreply.github.com>
2020-11-09 17:21:30 +0100

    Update init.R

--- init.R
+++ init.R
@@ -1,6 +1,6 @@
 
 # install packages if not already installed
-my_packages = c("forecast", "ggfortify", "shinydashboard", "plotly", "data.table", "lubridate", "tibble", "jsonlite", "httr")
+my_packages = c("forecast", "ggfortify", "shinydashboard", "plotly", "data.table", "lubridate", "tibble", "jsonlite", "httr", "DT")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
